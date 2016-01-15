dc3f4eb637fd2e1c41ff35a3c8e7ce4c36ffa87a
Ryan Shea <pachowstudios@gmail.com>
2016-01-14 21:52:16 -0800

    Improved authentication

--- JetSharp/Jet.cs
+++ JetSharp/Jet.cs
@@ -43,27 +43,40 @@ namespace JetSharp
       });
     }
 
+    public static async Task<Jet> CreateAsync([NotNull] string username, [NotNull] string password)
+    {
+      var instance = new Jet();
+
+      await instance.AuthenticateAsync(username, password);
+
+      return instance;
+    }
+
     public async Task AuthenticateAsync([NotNull] string username, [NotNull] string password)
     {
       Username = username;
       Password = password;
       Authenticator = new JetAuthenticator(
         await ExecuteRequestAsync<JetTokenRequest, JetToken>(
-          new JetTokenRequest(username, password), false)
-                .ConfigureAwait(false));
+          new JetTokenRequest(username, password)).ConfigureAwait(false));
     }
 
-    private async Task<TResponse> ExecuteRequestAsync<TRequest, TResponse>(TRequest request, bool authenticate = true)
+    private async Task<TResponse> ExecuteAuthenticatedRequestAsync<TRequest, TResponse>(TRequest request)
       where TRequest : JetRequest
     {
-      if (authenticate && (!Authenticator?.IsTokenValid ?? false))
+      if (!Authenticator?.IsTokenValid ?? false)
         await AuthenticateAsync(Username, Password).ConfigureAwait(false);
 
+      return await ExecuteRequestAsync<TRequest, TResponse>(request).ConfigureAwait(false);
+    }
+
+    private async Task<TResponse> ExecuteRequestAsync<TRequest, TResponse>(TRequest request)
+      where TRequest : JetRequest
+    {
       var response = await RestClient.ExecuteTaskAsync(
         new RestRequest(request.Resource, request.Method)
         { JsonSerializer = Serializer }
-          .AddJsonBody(request))
-        .ConfigureAwait(false);
+          .AddJsonBody(request)).ConfigureAwait(false);
 
       if (response.StatusCode == HttpStatusCode.Unauthorized)
         throw new AuthenticationException();
